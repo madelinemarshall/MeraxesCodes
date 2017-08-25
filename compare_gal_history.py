cc6d3a2c218c4c6b10075b19f5e3ec4932c2eb41
Madeline Marshall <mmarshal@sstar001.hpc.swin.edu.au>
2017-08-25 10:15:04 +1000

    Split compare_gal_history into 2 parts: store then compare

--- compare_gal_history.py
+++ compare_gal_history.py
@@ -1,54 +1,27 @@
 import numpy as np
 from dragons import meraxes
 import os
+import matplotlib.pyplot as plt
 import sys
 
-#####Below need to be the same as the one used in the SAM!!!####
-cosmo = {'omega_M_0' : 0.308, 
-'omega_lambda_0' : 0.692, 
-'omega_b_0' : 0.04839912, 
-'omega_n_0' : 0.0,
-'N_nu' : 0,
-'h' : 0.678,
-'n' : 0.968,
-'sigma_8' : 0.815
-}
+filename=[]
+nfiles=len(sys.argv)-1
+for ii in range(1,nfiles+1):
+  filename.append(sys.argv[ii])
 
-filename='bulges_crotonSF'
+hist={}
+for ii in range(0,nfiles):
+  hist["{}".format(filename[ii])]=np.load('gal_history_{}.npy'.format(filename[ii]))
+  if ('DiskScaleLength' in hist["{}".format(filename[ii])].dtype.names):
+    plt.plot(hist["{}".format(filename[ii])]['DiskScaleLength'],label=filename[ii])
+  else:
+    plt.plot(hist["{}".format(filename[ii])]['StellarDiskScaleLength'],label=filename[ii])
 
-fmeraxes = '/home/mmarshal/data_dragons/'+filename+'/output/meraxes.hdf5'
-snapshots, zs, lbts = meraxes.io.read_snaplist(fmeraxes,h=cosmo['h'])
-snapshot = 81
+plt.yscale('log')
+plt.xlabel('Snapshot')
+plt.ylabel('Disk Scale Length')
+plt.legend()
+plt.title('Evolution of Disk Size for Host of Most Massive BH Host at z=6')
+plt.show()
 
-gals,sim_props=meraxes.io.read_gals(fmeraxes, sim_props=True,\
-                                    snapshot=snapshot,h=cosmo['h'],quiet=True)
-props = gals.dtype.names 
-ID    = gals['ID']
-dr    = 70016445421
-index = np.where(ID==dr)
 
-gal_history = np.zeros(len(snapshots),dtype=gals.dtype)
-gal_history[snapshot] = gals[index]
-
-# history
-snap = snapshot
-tmp = index[0]
-while tmp>=0 and snap>0:
-    firstprogenitor = meraxes.io.read_firstprogenitor_indices(fmeraxes, snap)[tmp]
-    gal_history[snap-1] = meraxes.io.read_gals(fmeraxes,props=props,h =cosmo['h'],\
-                                snapshot=snap-1,quiet=True)[firstprogenitor]
-    tmp = firstprogenitor
-    snap-=1
-    
-#future
-snap = snapshot
-tmp = index[0]
-while tmp>=0 and snap<len(snapshots)-1:
-    descendant = meraxes.io.read_descendant_indices(fmeraxes, snap)[tmp]
-    gal_history[snap+1]=meraxes.io.read_gals(fmeraxes,props=props,h =cosmo['h'],\
-                                snapshot=snap+1,quiet=True)[descendant]
-    tmp = descendant
-    snap+=1    
-
-plt.plot(gal_history['DiskScaleLength'])
-plt.show()        
---
+++ store_MMBHgal_history.py
@@ -0,0 +1,57 @@
+## Determines and stores the history of the host of the most massive BH at z=6
+# (ID 70016445421 [adjustable]).
+# Input: name of the run folder which holds the Meraxes output to be used
+# Output: gal_history_[input_name].npy
+
+import numpy as np
+from dragons import meraxes
+import os
+import sys
+
+cosmo = {'omega_M_0' : 0.308, 
+'omega_lambda_0' : 0.692, 
+'omega_b_0' : 0.04839912, 
+'omega_n_0' : 0.0,
+'N_nu' : 0,
+'h' : 0.678,
+'n' : 0.968,
+'sigma_8' : 0.815
+}
+
+filename=str(sys.argv[1])
+
+fmeraxes = '/home/mmarshal/data_dragons/'+filename+'/output/meraxes.hdf5'
+snapshots, zs, lbts = meraxes.io.read_snaplist(fmeraxes,h=cosmo['h'])
+snapshot = 81
+
+gals,sim_props=meraxes.io.read_gals(fmeraxes, sim_props=True,\
+                                    snapshot=snapshot,h=cosmo['h'],quiet=True)
+props = gals.dtype.names 
+ID    = gals['ID']
+dr    = 70016445421
+index = np.where(ID==dr)
+
+gal_history = np.zeros(len(snapshots),dtype=gals.dtype)
+gal_history[snapshot] = gals[index]
+
+# history
+snap = snapshot
+tmp = index[0]
+while tmp>=0 and snap>0:
+    firstprogenitor = meraxes.io.read_firstprogenitor_indices(fmeraxes, snap)[tmp]
+    gal_history[snap-1] = meraxes.io.read_gals(fmeraxes,props=props,h =cosmo['h'],\
+                                snapshot=snap-1,quiet=True)[firstprogenitor]
+    tmp = firstprogenitor
+    snap-=1
+    
+#future
+snap = snapshot
+tmp = index[0]
+while tmp>=0 and snap<len(snapshots)-1:
+    descendant = meraxes.io.read_descendant_indices(fmeraxes, snap)[tmp]
+    gal_history[snap+1]=meraxes.io.read_gals(fmeraxes,props=props,h =cosmo['h'],\
+                                snapshot=snap+1,quiet=True)[descendant]
+    tmp = descendant
+    snap+=1    
+
+np.save('gal_history'+filename,gal_history)
